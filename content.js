579f11bf5c1d1c3cd526e307c5242da3d8718682
Bill Zito <bill.r.zito@gmail.com>
2016-10-17 16:14:57 -0700

    Render summarized content in hackernews html

--- content.js
+++ content.js
@@ -1,21 +1,21 @@
 //content.js
-// var $ = require('jquery');
-
-// $.ajax({
-// 			url: "https://hacker-news.firebaseio.com/v0/item/11118900.json",
-// 			type: "GET", 
-// 			contentType: "application/json",
-// 			success: function(data) {
-// 				console.log('made a call');
-// 				console.log('data', data);
-// 			},
-// 			error: function(err) {
-// 				console.log('found an err', err);
-// 			}
-// });
 
 $('.athing').each(function(i, item){
-		var test = '<tr><td><a href="#" class="storylink"> hello world </a></td></tr>';
-		$('.itemList > tbody > tr').eq(i*4).after(test);
-		console.log(i, 'success');
+	$.ajax({
+			url: "https://murmuring-citadel-29703.herokuapp.com/",
+			type: "GET", 
+			contentType: "application/json",
+			success: function(data) {
+				// console.log('made a call');
+				var summary = data[0];
+				// console.log(summary);
+				var test = '<tr class="subtext"><td colspan="2"></td><td>' + summary + '</td></tr>';
+				$('.itemList > tbody > tr').eq(i*4).after(test);
+				console.log(i, 'success');
+			},
+			error: function(err) {
+				console.log('found an err', err);
+			}
+	});
+	
 });
